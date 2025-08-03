3b9f5274364cc9296555ce31d31e227d5e5fd3a6
freedom-developer <freedom_developer@163.com>
2025-08-04 00:17:54 +0800

    update

--- procfs/procfs.c
+++ procfs/procfs.c
@@ -1,234 +1,191 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/seq_file.h>
 #include <linux/errno.h>
 #include <linux/uaccess.h>
+#include <linux/list.h>
+
+#include "procfs.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("wushengbang");
 MODULE_DESCRIPTION("A simple Linux kernel module");
 MODULE_VERSION("0.1");
 
-#define _min(a, b) (a) < (b) ? (a) : (b)
+typedef int (*wsb_single_show_t)(struct seq_file *, void *);
 
-struct proc_dir_entry *test_pde;
-const char *test_name = "test";
+struct proc_dir_entry *proc_mkfile(const char *name, struct proc_dir_entry *parent, int *data);
 
-int data;
-const char *test_string = "this is a test string.";
-int arr_data[] = {1, 2, 3, 10, 100, 1000, 3000, 20};
-const char *arr_str[] = {
-    "test1",
-    "test2",
-    "test3",
-};
-
-static void *ad_start(struct seq_file *m, loff_t *pos)
+/////////// 一个元素的读
+// 一个整数的读
+static int wsb_single_show_i(struct seq_file *m, void *v)
 {
-    if (*pos >= sizeof(arr_data) / sizeof(int)) return NULL;
-    return (void *)&arr_data[*pos];
+    seq_printf(m, "%d\n", *(int *)m->private);
+    return 0;
 }
-
-static void *ad_next(struct seq_file *m, void *v, loff_t *pos)
+// 一个字符串的读
+static int wsb_single_show_s(struct seq_file *m, void *v)
 {
-    (*pos)++;
-    if (*pos >= sizeof(arr_data) / sizeof(int)) return NULL;
-    return (void *)&arr_data[*pos];
-}
-
-static void ad_stop(struct seq_file *m, void *v)
-{
-
-}
-
-static int ad_show(struct seq_file *m, void *_data)
-{
-    seq_printf(m, "%d\t", *(int *)_data);
+    seq_printf(m, "%s\n", (const char *)m->private);
     return 0;
 }
 
-
-struct seq_operations ad_ops = {
-    .start = ad_start,
-    .next = ad_next,
-    .stop = ad_stop,
-    .show = ad_show,
-};
-
-
-static void *so_start(struct seq_file *m, loff_t *pos)
+struct proc_dir_entry *proc_mkfile_single(const char *name, umode_t mode, struct proc_dir_entry *parent, 
+                                    int type, void *data, int (*_show)(struct seq_file *, void *))
 {
-    if (*pos >= sizeof(arr_str) / sizeof(char *)) return NULL;
-    return arr_str[*pos];
+    wsb_single_show_t show = _show;
+    if (!show)
+        show = type == PROC_FT_INT ? wsb_single_show_i : wsb_single_show_s;
+    return proc_create_single_data(name, mode, parent, show, data);
 }
-static void *so_next(struct seq_file *m, void *v, loff_t *pos)
+///// end: 一个元素的读
+
+////////// 数组元素的读
+typedef struct pde_data_array_s {
+    void *data;
+    int nr;
+    int type;
+    int ele_size;
+    int (*show)(struct seq_file *m, void *v);
+
+    struct proc_dir_entry *pde;
+    struct list_head list;
+} pde_data_array_t;
+
+#define SEQ2DATA(m) (m)->file->f_inode->i_private
+
+static void *pda_start(struct seq_file *m, loff_t *ppos)
 {
-    (*pos)++;
-    if (*pos >= sizeof(arr_str) / sizeof(char *)) return NULL;
-    return arr_str[*pos];
+    pde_data_array_t *pda = (pde_data_array_t *)SEQ2DATA(m);
+    if (*ppos >= pda->nr)
+        return NULL;
+    return pda->data + pda->ele_size * (*ppos);
 }
-
-static int so_show(struct seq_file *m, void *_data)
+static void *pda_next(struct seq_file *m, void *v, loff_t *ppos)
 {
-    const char *p = (const char *)_data;
-    seq_printf(m, "%s\t", p);
-    return 0;
+    *ppos++;
+    pde_data_array_t *pda = (pde_data_array_t *)SEQ2DATA(m);
+    if (*ppos >= pda->nr)
+        return NULL;
+    return pda->data + pda->ele_size * (*ppos);
 }
-
-struct seq_operations so = {
-    .start = so_start,
-    .next = so_next,
-    .stop = ad_stop,
-    .show = so_show,
-};
-
-// _data 必为NULL
-static int show_single_data(struct seq_file *m, void *_data)
+static void pda_stop(struct seq_file *m, void *v) { }
+static int pda_show(struct seq_file *m, void *v)
 {
-    seq_printf(m, "%d\n", data);
-    return 0;
-}
-
-static int show_single_string(struct seq_file *m, void *_data)
-{
-    const struct file *filp = m->file;
-    struct inode *inode = filp->f_inode;
-    const char *p = (const char *)inode->i_private;
-    seq_printf(m, "%s\n", p);
-    return 0;
-}
-
-int rw_data;
-struct proc_data_buf {
-    void *buf;
-    size_t size;
-    size_t count;
-};
-
-static int rw_data_open(struct inode *inode, struct file *filp)
-{
-    // 准备好数据空间
-    struct proc_data_buf *pdb = kmalloc(sizeof(*pdb), GFP_KERNEL);
-    if (!pdb) return -ENOMEM;
-    filp->private_data = pdb;
-    pdb->size = 128;
-    pdb->buf = kmalloc(pdb->size, GFP_KERNEL);
-    if (!pdb->buf) {
-        kfree(pdb);
-        return -ENOMEM;
+    pde_data_array_t *pda = (pde_data_array_t *)SEQ2DATA(m);
+    void *last_data = pda->data + (pda->nr - 1) * pda->ele_size;
+    
+    if (pda->show) {
+        int ret = pda->show(m, v);
+        if (ret)
+            return ret;
+        if (last_data = v)
+            seq_printf(m, "\n");
+        return 0;
     }
-    pdb->count = snprintf(pdb->buf, pdb->size, "%d\n", rw_data);
+    if (pda->type == PROC_FT_INT) {
+        seq_printf(m, "%d", *(int *)v);
+    } else {
+        seq_printf(m, "%s", (const char *)v);
+    }
+    if (v != last_data) {
+        seq_printf(m, ",\t");
+    }
+    return 0;
+}
+
+static const struct seq_operations pde_data_array_ops = {
+    .start = pda_start,
+    .next = pda_next,
+    .stop = pda_stop,
+    .show = pda_show,
+};
+
+LIST_HEAD(pda_head);
+struct proc_dir_entry *proc_mkfile_array(const char *name, umode_t mode, struct proc_dir_entry *parent,
+                                    int type, void *data, int nr, int ele_size, 
+                                    int (*show)(struct seq_file *, void *))
+{
+    pde_data_array_t *pda = kmalloc(sizeof(pde_data_array_t), GFP_KERNEL);
+    if (!pda)
+        return ERR_PTR(ENOMEM);
+    pda->data = data;
+    pda->nr = nr;
+    pda->type = type;
+    pda->ele_size = ele_size;
+    pda->show = show;
+    INIT_LIST_HEAD(&pda->list);
+
+    struct proc_dir_entry *pde = proc_create_seq_data(name, mode, parent, &pde_data_array_ops, pda);
+    if (!pde) {
+        kfree(pda);
+        return ERR_PTR(ENOMEM);
+    }
+    pda->pde = pde;
+    list_add_tail(&pda->list, &pda_head);
+
+    return pde;
+}
+
+void proc_remove_array(struct proc_dir_entry *pde)
+{
+    pde_data_array_t *pda;
+    int found = 0;
+    list_for_each_entry(pda, &pda_head, list) {
+        if (pda->pde == pde) {
+            found = 1;
+            break;
+        }
+    }
+    if (found) {
+        list_del(&pda->list);
+        kfree(pda);
+    }
+    proc_remove(pde);
+}
+///// end: 数组元素的读
+
+
+
+
+//////////////// 测试 ////////////
+struct proc_dir_entry *test, *test_arr;
+struct stu {
+    char name[16];
+    int age;
+};
+struct stu wsb;
+
+int da[] = {100, 2, 200, 3, 5, 6, 99, 110};
+
+
+int stu_show(struct seq_file *m, void *v)
+{
+    struct stu *s = (struct stu *)m->private;
+    seq_printf(m, "name: %s,\tage: %d\n", s->name, s->age);
+    return 0;
+}
+
+static int __init procfs_init(void) 
+{    
+    strncpy(wsb.name, "wushengbang", 16);
+    wsb.age = 39;
+    test = proc_mkfile_single_o("test", NULL, &wsb, stu_show);
+    if (!test) 
+        return -1;
+    test_arr = proc_mkfile_array_i("test_arr", NULL, da, sizeof(da) / sizeof(int));
+    if (!test_arr)
+        return -2;
     
     return 0;
 }
-static ssize_t rw_data_read(struct file *filp, char __user *buf, size_t size, loff_t *pos)
+
+static void __exit procfs_exit(void) 
 {
-    struct proc_data_buf *pdb = filp->private_data;
-    ssize_t copy = _min(pdb->count - *pos, size);
-    if (!buf) return -EINVAL;
-    if (copy <= 0) return 0;
-    if (copy_to_user(buf, (char *)pdb->buf + *pos, copy))
-        return -EFAULT;
-    *pos += copy;
-    return  copy;
-}
-static ssize_t rw_data_write(struct file *filp, const char __user *buf, size_t size, loff_t *pos)
-{
-    struct proc_data_buf *pdb = filp->private_data;
-    ssize_t copy = _min(pdb->size, size);
-    if (!buf) return -EINVAL;
-    if (copy <= 0) return 0;
-    if (copy_from_user(pdb->buf + *pos, buf, copy))
-        return -EFAULT;
-    *pos += copy;
-    pdb->count = *pos;
-    if (sscanf(pdb->buf, "%d", &rw_data) <= 0)
-        return -EINVAL;
-    return copy;
-}
-static int rw_data_release(struct inode *inode, struct file *filp)
-{
-    struct proc_data_buf *pdb = filp->private_data;
-    if (pdb->buf) {
-        kfree(pdb->buf);
-    }
-    kfree(pdb);
-    filp->private_data = NULL;
-    return 0;
-}
-
-struct proc_ops rw_data_ops = {
-    .proc_open = rw_data_open,
-    .proc_read = rw_data_read,
-    .proc_write = rw_data_write,
-    .proc_release = rw_data_release,
-};
-
-
-#define RW_STR_LEN 4096
-char rw_str[RW_STR_LEN];
-size_t rw_str_length;
-static int rw_str_open(struct inode *inode, struct file *filp)
-{
-    if (filp->f_flags & O_APPEND)
-        filp->f_pos = rw_str_length;
-    return 0;
-}
-static ssize_t rw_str_read(struct file *filp, char __user *buf, size_t size, loff_t *pos)
-{
-    ssize_t copy = _min(rw_str_length - *pos, size);
-    if (copy <= 0) return 0;
-    if (copy_to_user(buf, rw_str + *pos, copy)) return -EFAULT;
-    *pos += copy;
-    return copy;
-}
-static ssize_t rw_str_write(struct file *filp, const char __user *buf, size_t size, loff_t *pos)
-{
-    ssize_t copy = _min(RW_STR_LEN - *pos, size);
-    if (copy <= 0) return 0;
-    if (copy_from_user(rw_str + *pos, buf, copy)) return -EFAULT;
-    *pos += copy;
-    rw_str_length = *pos;
-    return copy;
-}
-struct proc_ops rw_str_ops = {
-    .proc_open = rw_str_open,
-    .proc_read = rw_str_read,
-    .proc_write = rw_str_write,
-};
-
-static int __init procfs_init(void) {
-    printk(KERN_INFO "Hello, kernel world!\n");
-    test_pde = proc_mkdir(test_name, NULL);
-    if (!test_pde) return -1;
-
-    data = 1009;
-    struct proc_dir_entry *single_file = proc_create_single_data("data", 0444, test_pde, show_single_data, NULL);
-    if (!single_file) return -1;
-
-    struct proc_dir_entry *test_string_pde = proc_create_single_data("test_string", 0444, test_pde, show_single_string, (void *)test_string);
-    if (!test_string_pde) return -1;
-
-    struct proc_dir_entry *arr_data_pde = proc_create_seq("arr_data", 0444, test_pde, &ad_ops);
-    if (!arr_data_pde) return -1;
-
-    struct proc_dir_entry *arr_str_pde = proc_create_seq("arr_str", 0444, test_pde, &so);
-    if (!arr_str_pde) return -1;
-
-    struct proc_dir_entry *rw_data_pde = proc_create("rw_data", 0666, test_pde, &rw_data_ops);
-    if (!rw_data_pde) return -1;
-
-    struct proc_dir_entry *rw_str_pde = proc_create("rw_str", 0666, test_pde, &rw_str_ops);
-    if (!rw_str_pde) return -1;
-
-    return 0;
-}
-
-static void __exit procfs_exit(void) {
-    printk(KERN_INFO "Goodbye, kernel world!\n");
-    proc_remove(test_pde);
+    if (test)
+        proc_remove(test);
+    if (test_arr)
+        proc_remove_array(test_arr);
 }
 
 module_init(procfs_init);
---
+++ procfs/procfs.h
@@ -0,0 +1,31 @@
+#ifndef _WSB_PROCFS_H
+#define _WSB_PROCFS_H
+
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+#define PROC_FT_INT 1
+#define PROC_FT_STR 2
+#define PROC_FT_OTH 3
+
+struct proc_dir_entry *proc_mkfile_single(const char *name, umode_t mode, struct proc_dir_entry *parent, 
+                                    int type, void *data, int (*_show)(struct seq_file *, void *));
+#define proc_mkfile_single_i(name, parent, data) \
+    proc_mkfile_single((name), 0, (parent), PROC_FT_INT, (data), NULL)
+#define proc_mkfile_single_s(name, parent, data) \
+    proc_mkfile_single((name), 0, (parent), PROC_FT_STR, (data), NULL)
+#define proc_mkfile_single_o(name, parent, data, show) \
+    proc_mkfile_single((name), 0, (parent), PROC_FT_OTH, (data), (show))
+
+
+struct proc_dir_entry *proc_mkfile_array(const char *name, umode_t mode, struct proc_dir_entry *parent,
+                                    int type, void *data, int nr, int ele_size,
+                                    int (*show)(struct seq_file *, void *v));
+#define proc_mkfile_array_i(name, parent, data, nr) \
+    proc_mkfile_array((name), 0, (parent), PROC_FT_INT, (data), (nr), sizeof(int), NULL)
+#define proc_mkfile_array_s(name, parent, data, nr) \
+    proc_mkfile_array((name), 0, (parent), PROC_FT_STR, (data), (nr), sizeof(char *), NULL)
+#define proc_mkfile_array_o(name, parent, data, nr, ele_size, show) \
+    proc_mkfile_array((name), 0, (parent), PROC_FT_OTH, (data), (nr), ele_size, show)
+
+#endif
